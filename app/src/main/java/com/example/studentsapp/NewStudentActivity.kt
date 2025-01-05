b242010a34a3eedd51a5887a0e4c2f424e6fcb3d
marlenbd <marlen@htbioimaging.com>
2025-01-05 22:26:37 +0200

    add fields to add studenr + cancel button

--- app/src/main/java/com/example/studentsapp/NewStudentActivity.kt
+++ app/src/main/java/com/example/studentsapp/NewStudentActivity.kt
@@ -4,6 +4,7 @@ import Student
 import androidx.activity.ComponentActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 
 class NewStudentActivity : ComponentActivity() {
@@ -14,16 +15,28 @@ class NewStudentActivity : ComponentActivity() {
 
         val studentNameInput: EditText = findViewById(R.id.student_name_input)
         val studentIdInput: EditText = findViewById(R.id.student_id_input)
+        val studentPhoneInput: EditText = findViewById(R.id.student_phone_input)
+        val studentAddressInput: EditText = findViewById(R.id.student_address_input)
+        val checkBox: CheckBox = findViewById(R.id.check_status)
+
         val addButton: Button = findViewById(R.id.add_student_button)
 
         addButton.setOnClickListener {
             val name = studentNameInput.text.toString()
             val id = studentIdInput.text.toString()
-
-            val newStudent = Student(name, id, false, R.drawable.student_image)
+            val phone = studentPhoneInput.text.toString()
+            val address = studentAddressInput.text.toString()
+            val isChecked = checkBox.isChecked
+            val newStudent = Student(name, id, phone, address, isChecked, R.drawable.student_image)
 
             StudentRepository.addStudent(newStudent)
 
+            finish()
+        }
+
+        val cancelButton: Button = findViewById(R.id.cancel_add_student_button)
+
+        cancelButton.setOnClickListener {
             finish()
         }
     }
