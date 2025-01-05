1eb2f5f944c3efb78a712b79480453a9ad333c0c
marlenbd <marlen@htbioimaging.com>
2025-01-05 22:44:21 +0200

    add fields to student details activity

--- app/src/main/java/com/example/studentsapp/StudentDetailsActivity.kt
+++ app/src/main/java/com/example/studentsapp/StudentDetailsActivity.kt
@@ -2,6 +2,7 @@ package com.example.studentsapp
 
 import Student
 import android.os.Bundle
+import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.ComponentActivity
@@ -15,11 +16,17 @@ class StudentDetailsActivity : ComponentActivity() {
         val studentNameTextView: TextView = findViewById(R.id.student_name)
         val studentIdTextView: TextView = findViewById(R.id.student_id)
         val studentImageView: ImageView = findViewById(R.id.student_image)
+        val studentPhoneView: TextView = findViewById(R.id.student_phone)
+        val studentAddressView: TextView = findViewById(R.id.student_address)
+        val studentCheckView: CheckBox = findViewById(R.id.check_status)
 
         val student = intent.getParcelableExtra<Student>("STUDENT_KEY")
 
         studentNameTextView.text = student?.name
         studentIdTextView.text = student?.id
+        studentPhoneView.text = student?.phone
+        studentAddressView.text = student?.address
+        studentCheckView.isChecked = student?.isChecked == true
         studentImageView.setImageResource(student?.imageResId ?: R.drawable.student_image)
     }
 }
