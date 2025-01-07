f7d5ffd71cb1d51a6f8fa5f8ca4f14f977616de3
marlenbd <marlen@htbioimaging.com>
2025-01-07 18:29:15 +0200

    move details init to function

--- app/src/main/java/com/example/studentsapp/StudentDetailsActivity.kt
+++ app/src/main/java/com/example/studentsapp/StudentDetailsActivity.kt
@@ -17,21 +17,9 @@ class StudentDetailsActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student_details)
 
-        val studentNameTextView: TextView = findViewById(R.id.student_name)
-        val studentIdTextView: TextView = findViewById(R.id.student_id)
-        val studentImageView: ImageView = findViewById(R.id.student_image)
-        val studentPhoneView: TextView = findViewById(R.id.student_phone)
-        val studentAddressView: TextView = findViewById(R.id.student_address)
-        val studentCheckView: CheckBox = findViewById(R.id.check_status)
-
         val student = intent.getParcelableExtra<Student>("STUDENT_KEY")
 
-        studentNameTextView.text = student?.name
-        studentIdTextView.text = student?.id
-        studentPhoneView.text = student?.phone
-        studentAddressView.text = student?.address
-        studentCheckView.isChecked = student?.isChecked == true
-        studentImageView.setImageResource(student?.imageResId ?: R.drawable.student_image)
+        initStudentDetails(student!!)
 
         val editButton: Button = findViewById(R.id.edit_student_button)
 
@@ -51,24 +39,29 @@ class StudentDetailsActivity : ComponentActivity() {
                 finish()
             } else {
                 val updatedStudent = data?.getParcelableExtra<Student>("updatedStudent")
-
-                val studentNameTextView: TextView = findViewById(R.id.student_name)
-                val studentIdTextView: TextView = findViewById(R.id.student_id)
-                val studentImageView: ImageView = findViewById(R.id.student_image)
-                val studentPhoneView: TextView = findViewById(R.id.student_phone)
-                val studentAddressView: TextView = findViewById(R.id.student_address)
-                val studentCheckView: CheckBox = findViewById(R.id.check_status)
-
-                updatedStudent?.let {
-                    // Update the UI with the new student details
-                    studentNameTextView.text = it.name
-                    studentIdTextView.text = it.id
-                    studentPhoneView.text = it.phone
-                    studentAddressView.text = it.address
-                    studentCheckView.isChecked = it.isChecked
-                    studentImageView.setImageResource(it.imageResId ?: R.drawable.student_image)
+                if (updatedStudent != null ) {
+                    initStudentDetails(updatedStudent)
                 }
             }
+        }
+    }
+
+    fun initStudentDetails(student: Student) {
+        val studentNameTextView: TextView = findViewById(R.id.student_name)
+        val studentIdTextView: TextView = findViewById(R.id.student_id)
+        val studentImageView: ImageView = findViewById(R.id.student_image)
+        val studentPhoneView: TextView = findViewById(R.id.student_phone)
+        val studentAddressView: TextView = findViewById(R.id.student_address)
+        val studentCheckView: CheckBox = findViewById(R.id.check_status)
+
+        student?.let {
+            // Update the UI with the new student details
+            studentNameTextView.text = it.name
+            studentIdTextView.text = it.id
+            studentPhoneView.text = it.phone
+            studentAddressView.text = it.address
+            studentCheckView.isChecked = it.isChecked
+            studentImageView.setImageResource(it.imageResId ?: R.drawable.student_image)
         }
     }
 }
